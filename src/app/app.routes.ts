16e79290017242e5985068cff2ea7f4be36de242
Dimitriadis Nicolas <dimitriadisnicolas@gmail.com>
2025-12-18 10:44:26 +0100

    add Tailwind CSS integration and service worker support

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./home/home.component').then((m) => m.HomeComponent),
+    children: [
+      {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full'
+      },
+      {
+        path: 'dashboard',
+        loadComponent: () =>
+          import('./dashboard/dashboard.component').then((m) => m.DashboardComponent),
+
+      },
+      {
+        path:'calendar',
+        loadComponent: () =>
+          import('./calendar/calendar.component').then((m) => m.CalendarComponent),
+      }
+    ]
+  }
+
+
+
+];
